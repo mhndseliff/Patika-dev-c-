72ee3fde4692c6ffee876682ccccfa2258a0d410
Elif Yildiz <elif.yildiz@paycore.com>
2021-09-21 12:58:54 +0300

    console app

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace programlama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+         Console.WriteLine("İsminizi Giriniz");
+         string name = Console.ReadLine();
+         Console.WriteLine("Soyadınızı Giriniz");
+         string surname= Console.ReadLine();
+        
+         Console.WriteLine("Merhaba Memnun Oldum " + name + " " + surname);
+         Console.ReadKey();
+
+                  
+        }
+    }
+}
